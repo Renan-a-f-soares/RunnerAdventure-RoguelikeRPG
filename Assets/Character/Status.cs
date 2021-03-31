498f44f79649a7d16121083bb2947cc5531df6ed
Renan.a.f.soares <renan.a.f.soares@gmail.com>
2021-03-31 11:31:24 -0300

    Atualização de status dos personagens

---
+++ Assets/Character/Status.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour
+{
+    //Miscelanias
+    public string unitName;
+    public int level;
+    public float currentXP;
+    public float nextLevelXP;
+    public int improvementPoints;
+
+    //Status
+    public int strengt;
+    public int dexterity;
+    public int constitution;
+    public int inteligence;
+    public int experience;
+    public int magic;
+
+
+    //Recursos
+    public float currentHP;
+    public float maxHP;
+
+    public float currentMP;
+    public float maxMP;
+
+    public float currentShield;
+    public float maxShield;
+
+    public int armor;
+}
