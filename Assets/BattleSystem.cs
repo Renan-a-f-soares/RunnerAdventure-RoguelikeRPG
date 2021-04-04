9b5e1457ed0400200f9cd8ee5b2ec91ad00bab1b
Renan.a.f.soares <renan.a.f.soares@gmail.com>
2021-04-04 20:22:49 -0300

    Atualização Status

--- Assets/BattleSystem.cs
+++ Assets/BattleSystem.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-
-[SerializeField] 
 public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST};
 
 public class BattleSystem : MonoBehaviour
@@ -21,32 +19,124 @@ public class BattleSystem : MonoBehaviour
     //Instancia as propriedades do Script Status
     Status playerUnit;
     Status enemyUnit;
+    Status stateTarget;
+    Status stateSelf;
+    UiManager uiTarget;
 
     //Instanciar o texto para a caixa de mensagem principal
     public TMP_Text dialogMessage;
+
+
+    public UiManager player_StatusBox;
+    public UiManager enemy_StatusBox;
 
     public BattleState state;
 
     void Start()
     {
         state = BattleState.START;
-        StartBattle();
+        StartCoroutine(StartBattle());
     }
     
-    void StartBattle()
+    IEnumerator StartBattle()
     {
-
         //Tranforma o inimigo e o jogador em um game object e extrai os dados do script "Status"
         GameObject playerinstance = Instantiate(playerTemplate, playerOrigin);
         playerUnit = playerinstance.GetComponent<Status>();
         GameObject enemyInstance = Instantiate(enemyTemplate, enemyOrigin);
         enemyUnit = enemyInstance.GetComponent<Status>();
 
+
         dialogMessage.text = "Um "+ enemyUnit.unitName + " Esta pronto para te atacar.";
 
+        
+
+        player_StatusBox.SetStatus(playerUnit);
+        enemy_StatusBox.SetStatus(enemyUnit);
+
+        yield return new WaitForSeconds(2.5f);
+        state = BattleState.PLAYERTURN;
+        StartCoroutine(PlayerTurn());
+
+
+    }
+
+    void FixedUpdate()
+    {
+        if(state == BattleState.PLAYERTURN)
+        {
+            stateTarget = enemyUnit;
+            stateSelf = playerUnit;
+            uiTarget = enemy_StatusBox;
+        }
+        else if(state == BattleState.ENEMYTURN)
+        {
+            stateTarget = playerUnit;
+            stateSelf = enemyUnit;
+            uiTarget = player_StatusBox;
+        }
+    }
+    
+
+    IEnumerator PlayerTurn()
+    {
+        yield return new WaitForSeconds(1.5f);
+        dialogMessage.text = "o que voce fará?";
+        
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        PlayAttack();
+        yield return new WaitForSeconds(0.1f);
+        state = BattleState.PLAYERTURN;
+        StartCoroutine(PlayerTurn());
+
+    }
+
+    IEnumerator PlayAttack()
+    {
+        yield return new WaitForSeconds(0.5f);
+        stateTarget.TakeDamage(stateSelf.UpdateDamage());
+        StartCoroutine(DamageCount(stateSelf.UpdateDamage()));
+        //uiTarget.DamageDealer(stateTarget.currentHP);
+        if (state == BattleState.PLAYERTURN)
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+        uiTarget.StatusUpdate(stateTarget);
+    }
+
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN) { return; }
+        StartCoroutine(PlayAttack());
+    }
+
+    public IEnumerator ArmorBlock(int hit)
+    {
+        if (hit == 1)
+        {
+            yield return new WaitForSeconds(1.5f);
+            dialogMessage.text = "O Ataque perfura a armadura !";
+        }
+        else
+        {
+            yield return new WaitForSeconds(1.5f);
+            dialogMessage.text = "A armadura Bloqueou o Ataque !";
+        }
+
+    }
+
+    public IEnumerator DamageCount(float hurtDamage)
+    {
+        
+        
+        dialogMessage.text = stateSelf.unitName +" Desferiu " + hurtDamage +"!";
+        yield return new WaitForSeconds(1.5f);
     }
 
 
 
-    
 }
--- Assets/Character/Status.cs
+++ Assets/Character/Status.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Status : MonoBehaviour
 {
+
+    BattleSystem sendMessage;
+    
+    //Variaveis para os Status
     //Miscelanias
     public string unitName;
     public int level;
-    public string strLevel;
     public float currentXP;
     public float nextLevelXP;
     public int improvementPoints;
@@ -20,6 +23,15 @@ public class Status : MonoBehaviour
     public int experience;
     public int magic;
 
+    //modificadores
+
+    public float strengtMod;
+    public float dexteritymod;
+    public float constitutionMod;
+    public float inteligenceMod;
+    public float experienceMod;
+    public float magicMod;
+
 
     //Recursos
     public float currentHP;
@@ -28,9 +40,79 @@ public class Status : MonoBehaviour
     public float currentMP;
     public float maxMP;
 
+    public string shieldText;
     public float currentShield;
     public float maxShield;
 
     public int currentArmor;
     public int maxArmor;
+
+    //equipamentos
+    public int weaponAttack = 20;
+    public int armorDefense;
+
+
+    //calculo de dano basico de ataque
+
+    public float damage;
+
+    //Controladores
+    public int hitChance;
+    public int hit;
+    public int armorQuality = 26;
+
+    public float UpdateDamage()
+    {
+        strengtMod = strengt/50;
+        experienceMod = experience / 50;
+        magicMod = magic * (magic / 50);
+
+        damage = (weaponAttack*(1+strengtMod))+(magicMod*(1+experienceMod));
+        
+        return damage;
+    }
+    public void TakeDamage(float hurtDamage)
+    {
+        
+        //armadura
+        if (currentArmor > 0)
+        {
+            hitChance = Random.Range(1, 101);
+            if (hitChance < armorQuality)
+            {
+                hurtDamage = 0;
+                hit = 0;
+            }
+            else
+            {
+                hit = 1;
+            }
+            
+            currentArmor -= 1;
+        }
+        
+        //escudo magico
+        if (currentShield != 0)
+        {
+            if (currentShield > hurtDamage)
+            {
+                currentShield -= hurtDamage;
+                hurtDamage = 0;
+                shieldText = "+" + currentShield;
+            }
+            else
+            {
+                hurtDamage = hurtDamage - currentShield;
+                currentShield = 0;
+                shieldText = "";
+
+
+            }
+        }
+        currentHP -= hurtDamage;
+        
+        
+
+       
+    }
 }
